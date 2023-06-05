126f82b7c9484dbf90797e5b19575a1f2f864d1c
Varun Kumar <130556802+varunpracticegit@users.noreply.github.com>
2023-06-05 11:31:25 +0530

    Add files via upload

---
+++ ChatApp/room/views.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+from .models import Room, Message
+
+@login_required
+def rooms(request):
+    rooms = Room.objects.all()
+
+    return render(request, 'room/rooms.html', {'rooms': rooms})
+
+@login_required
+def room(request, slug):
+    room = Room.objects.get(slug=slug)
+    messages = Message.objects.filter(room=room)[0:100]
+    users = room.users.all()
+
+    return render(request, 'room/room.html', {'room': room, 'messages': messages, 'users': users})
